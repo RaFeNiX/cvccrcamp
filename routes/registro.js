1c29c77df6284c4bcacdd3eda2451f4fd71bbe4d
rodbalbino <rodrigodasilva@cvc.com.br>
2017-09-22 21:00:27 -0300

    Listagem de cards

--- routes/registro.js
+++ routes/registro.js
@@ -1,25 +1,44 @@
-var express = require('express');
-var router = express.Router();
-var bodyParser = require('body-parser');
-var participantes = require('../modules/register')
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const participantes = require('../modules/register');
+const _ = require('lodash');
+
+const fs = require('fs');
+var cards = [];
+
+fs.readdir('./public/images/cards/', (err, files) => {
+    cards = _.map(files, (cardName) => {
+        return {
+            name: cardName,
+            isUsed: _.random(true, false)
+        }
+    });
+})
+
 router.use(bodyParser.urlencoded({ extended: true }));
 
 /* GET registro listing. */
 router.get('/', function (req, res, next) {
     res.render('registro', {
-        title: 'Cadastro'
+        title: 'Cadastro',
+        cards: cards
     });
 });
 
 router.post('/', function (req, res) {
+
     participantes.insert({
         nome: req.body.nome,
+        carta: req.body.carta,
         email: req.body.email,
         idClash: req.body.idClash
     })
 
     console.log("Registrado com sucesso");
     res.redirect('/registro');
+
 });
+
 
 module.exports = router;
